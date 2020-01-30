420ef0a2e2395200a14272cf73efe6de1d3eb7c6
Marko Vitković <mavitkov@vsite.hr>
2020-01-30 21:47:03 +0100

    Add project files.

---
+++ Stadium_webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Stadium_webapp.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
