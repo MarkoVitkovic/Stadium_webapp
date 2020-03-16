6646a32f1c5467a0be00379eee5c7f62a0d3e75a
Marko Vitković <56235017+MarkoVitkovic@users.noreply.github.com>
2020-03-16 22:59:28 +0100

    Add files via upload

---
+++ dbo.stadiums.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[stadiums] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (MAX) NULL,
+    [ClubName]    NVARCHAR (MAX) NULL,
+    [TicketPrice] DECIMAL          NOT NULL,
+    [Capacity]    INT            NOT NULL,
+    CONSTRAINT [PK_stadiums] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
